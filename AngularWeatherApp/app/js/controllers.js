dd60b93817282943a808b14c9c075836e3665f23
jrisk <joeyrsk@gmail.com>
2015-04-22 17:03:21 -0400

    initial angular bindings

--- AngularWeatherApp/app/js/controllers.js
+++ AngularWeatherApp/app/js/controllers.js
@@ -7,8 +7,8 @@ var weatherApp = angular.module('weatherApp', []);
 weatherApp.controller('weatherController', ['$scope', '$http', function($scope, $http) {
 	$http.get('http://api.openweathermap.org/data/2.5/weather?q=Virginia%20Beach,VA').success(function(data) {
 		$scope.reports = data;
-		
 	});
+	
 }]);
 	
 
